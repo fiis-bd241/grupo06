f37e1b1621ee0dc2a556443b947741b472421d77
Gianfranco Solier <123434246+Ganimbus@users.noreply.github.com>
2024-06-26 12:28:15 -0500

    Update urls.py alm_central

--- Entregable 4/proyecto/backend/vircatex/almacen_central/urls.py
+++ Entregable 4/proyecto/backend/vircatex/almacen_central/urls.py
@@ -3,10 +3,10 @@ from . import views
 from .views import LotesEntreFechasView, ProveedorMateriaPrimaView, CrearProveedorView
 
 urlpatterns = [
-    path('lotes/', views.LoteListView.as_view(), name='lote-list'),
-    path('lotes_entre_fechas/', LotesEntreFechasView.as_view()),
-    path('proveedor_materia_prima/', ProveedorMateriaPrimaView.as_view()),
-    path('lote_entrada/', views.LotesEntradaView.as_view()),
-    path('lote_salida/', views.LotesSalidaView.as_view()),
-    path('crear_proveedor/', CrearProveedorView.as_view()),
+    path('almacen_central/lotes/', views.LoteListView.as_view(), name='lote-list'),
+    path('almacen_central/lotes_entre_fechas/', LotesEntreFechasView.as_view()),
+    path('almacen_central/proveedor_materia_prima/', ProveedorMateriaPrimaView.as_view()),
+    path('almacen_central/lote_entrada/', views.LotesEntradaView.as_view()),
+    path('almacen_central/lote_salida/', views.LotesSalidaView.as_view()),
+    path('almacen_central/crear_proveedor/', CrearProveedorView.as_view()),
 ]
