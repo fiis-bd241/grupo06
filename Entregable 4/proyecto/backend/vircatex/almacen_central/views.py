ff47a87cab6ad80983aede49fdc721f4b8a666a7
williamvalenciaK <william.valencia.g@uni.pe>
2024-06-26 02:49:43 -0500

    Esqueleto de código backend inicial

---
+++ Entregable 4/proyecto/backend/vircatex/almacen_central/views.py
@@ -0,0 +1,4 @@
+from django.db import connection
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from core.models import *
---
+++ Entregable 4/proyecto/backend/vircatex/almacen_transito/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlmacenTransitoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'almacen_transito'
