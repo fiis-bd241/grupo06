612a3c49841858452228615e0f1a37a98ecee1ab
williamvalenciaK <william.valencia.g@uni.pe>
2024-06-29 18:07:50 -0500

    Centralización de urls

--- Entregable 4/proyecto/backend/vircatex/urls.py
+++ Entregable 4/proyecto/backend/vircatex/urls.py
@@ -38,12 +38,16 @@ urlpatterns = [
 
     # almacen central
     path('almacen_central/', include([
-        path('lotes/', LoteListView.as_view(), name='lote-list'),
-        path('lotes_entre_fechas/', LotesEntreFechasView.as_view()),
-        path('proveedor_materia_prima/', ProveedorMateriaPrimaView.as_view()),
-        path('lote_entrada/', LotesEntradaView.as_view()),
-        path('lote_salida/', LotesSalidaView.as_view()),
-        path('crear_proveedor/', CrearProveedorView.as_view()),
+        path('almacen_central/lotes/', LoteListView.as_view(), name='lote-list'),
+        path('almacen_central/lotes_entre_fechas/', LotesEntreFechasView.as_view()),
+        path('almacen_central/proveedor_materia_prima/', ProveedorMateriaPrimaView.as_view()),
+        path('almacen_central/lote_entrada/', LotesEntradaView.as_view()),
+        path('almacen_central/lote_salida/', LotesSalidaView.as_view()),
+        path('almacen_central/proveedor/crear_o_modificar/', CrearProveedorView.as_view()),
+        path('almacen_central/proveedor/eliminar/<str:ruc>/', EliminarProveedorView.as_view()),
+        path('almacen_central/lote/crear/', CrearLoteView.as_view()),
+        path('almacen_central/lote_entrada/crear/', CrearLoteEntradaView.as_view()),
+        path('almacen_central/lote_salida/mover/', MoverLoteSalidaView.as_view()),
     ])),
 
     # almacen de tránsito
