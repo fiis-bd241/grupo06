f7b1a7932e2f2447a989d6f60449a4e7da9ee95a
Gianfranco Solier <123434246+Ganimbus@users.noreply.github.com>
2024-06-29 17:15:25 -0500

    Update urls.py

--- Entregable 4/proyecto/backend/almacen_central/urls.py
+++ Entregable 4/proyecto/backend/almacen_central/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('almacen_central/proveedor_materia_prima/', ProveedorMateriaPrimaView.as_view()),
     path('almacen_central/lote_entrada/', views.LotesEntradaView.as_view()),
     path('almacen_central/lote_salida/', views.LotesSalidaView.as_view()),
-    path('almacen_central/crear_proveedor/', CrearProveedorView.as_view()),
+    path('almacen_central/proveedor/crear_o_modificar/', CrearProveedorView.as_view()),
+    path('almacen_central/proveedor/eliminar/<str:ruc>/', EliminarProveedorView.as_view()),
+    path('almacen_central/lote/crear/', CrearLoteView.as_view()),
+    path('almacen_central/lote_entrada/crear/', CrearLoteEntradaView.as_view()),
+    path('almacen_central/lote_salida/mover/', MoverLoteSalidaView.as_view()),
 ]
